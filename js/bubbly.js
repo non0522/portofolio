3908c44ac76080dc0d03f9d781376444119ca59a
Nozomu Dokoh <tasty.nuts.d@gmail.com>
2021-08-08 17:27:10 +0900

    post portfolio

---
+++ js/bubbly.js
@@ -0,0 +1,14 @@
+bubbly({
+  blur:15,
+  colorStart: "#fff",
+  colorStop: "#fff",
+  // shadowColor: "#5DB09B",
+  compose: "source-over",
+  radiusFunc:() => 1 + Math.random() * 25,
+  angleFunc:() => -Math.PI / 2,
+  velocityFunc:() => Math.random() * 2,
+  bubbleFunc: () => `hsla(160, 30%, 80%, ${Math.random() * .5})`,
+  // shadowColor: "#5DB09B",
+  bubbles:150
+});
+
---
+++ js/style.js
@@ -0,0 +1,41 @@
+// ==========================================================================
+//トップページ、タイプライター風に表示
+$(window).bind('load',function(){
+  // ここで文字を<span></span>で囲む
+  $('.js-typ').children().andSelf().contents().each(function() {
+  if (this.nodeType == 3) {
+  $(this).replaceWith($(this).text().replace(/(\S)/g, '<span>$1</span>'));
+  }
+  });
+  // 一文字ずつフェードインさせる
+  $('.js-typ').css({'opacity':1});
+  for (let i = 0; i <= $('.js-typ').children().size(); i++) {
+  $('.js-typ').children('span:eq('+i+')').delay(100*i).animate({'opacity':1},50);
+  };
+  });
+
+// ==========================================================================
+//グラフのanimation、見えたら発火
+  $(window).scroll(function(){
+    var top = $(".js-animation").offset().top; // ターゲットの位置取得
+    var position = top - $(window).height();  // 発火させたい位置
+    if($(window).scrollTop() > position){
+      $(".js-animation").css("animation", "expand 1.5s ease");
+    }else{
+      $(".js-animation").css("animatin", "none");
+    }
+    
+  })
+
+
+  jQuery( function( $ ) {
+    $( ".js-hamburger" ).on( "click", function() {
+      $( this ).toggleClass( "is-open" );
+      $( ".p-gnav" ).toggleClass( "is-open" );
+      $( "body" ).toggleClass( "is-open" );
+    } );
+  } );
+
+  $('.p-gnav a[href]').on('click', function(event) {
+    $('.js-hamburger').trigger('click');
+});
