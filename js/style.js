869c187b9b813054907cdda8b7c4e68d9fe1ab67
Nozomu Dokoh <tasty.nuts.d@gmail.com>
2021-08-09 19:06:28 +0900

    changed images

--- js/style.js
+++ js/style.js
@@ -40,6 +40,10 @@ $(window).bind('load',function(){
 
 // ==========================================================================
 //ハンバーガーボタンオープン時、メニューをクリックした時にメニューを閉じる
+$('.p-gnav a[href]').on('click', function(event) {
+  $('.js-hamburger').trigger('click');
+});
+
 
 $(function(){	
   $(window).scroll(function(){
@@ -61,81 +65,57 @@ $(".list").css("color", "#000");
 }); 
 
 
-//   $('.p-gnav a[href]').on('click', function(event) {
-//     $('.js-hamburger').trigger('click');
-// });
 
 
-// function changeColor(idname){
-//   var obj = document.getElementById(idname);
-//   obj.style.color = '#ffffff';            //文字色を白にする
-//   obj.style.backgroundColor = '#ff0000';  //背景色を赤にする
 
 
-// ==========================================================================
-//ハンバーガーボタン用jQuery
-// jQuery( function( $ ) {
-//   $( ".js-slide" ).hover (function() {
-//     $( this ).toggleClass( "is-appear" );
-//     $( ".c-diagram__middle" ).toggleClass( "is-appear" );
-//   } );
-// } );
+//スクロールした際の動きを関数でまとめる
+function setFadeElement(){
+  var windowH = $(window).height(); //ウィンドウの高さを取得
+  var scroll = $(window).scrollTop(); //スクロール値を取得
+    
+    //出現範囲の指定
+  var contentsTop = Math.round($('.js-sec3').offset().top);  //要素までの高さを四捨五入した値で取得
+  var contentsH = $('.js-sec3').outerHeight(true); //要素の高さを取得
+    
+    //2つ目の出現範囲の指定※任意
+  //var contentsTop2 = Math.round($('#area-5').offset().top); //要素までの高さを取得
+  //var contentsH2 = $('#area-5').outerHeight(true);//要素の高さを取得
 
-// $(function () {
-//   $(window).on("scroll", function () {
-//     const sliderHeight = $(".js-change-1").height();
-//     if (sliderHeight - 30 < $(this).scrollTop()) {
-//       $(".js-gnav").addClass("is-white");
-//     } else {
-//       $(".js-gnav").removeClass("is-white");
-//     }
-//   });
-// });
+    //出現範囲内に入ったかどうかをチェック
+  if(scroll+windowH >= contentsTop && scroll+windowH  <= contentsTop+contentsH * 3){
+    $("#page-top").addClass("UpMove");    //入っていたらUpMoveをクラス追加
+    $("#page-top").removeClass("DownMove");   //DownMoveを削除
+    $(".hide-btn").removeClass("hide-btn");   //hide-btnを削除
+  }//2つ目の出現範囲に入ったかどうかをチェック※任意
+   // else if(scroll+windowH >= contentsTop2 && scroll+windowH <= contentsTop2+contentsH2){       
+    //$("#page-top").addClass("UpMove");    //入っていたらUpMoveをクラス追加
+    //$("#page-top").removeClass("DownMove");   //DownMoveを削除
+  //}//それ以外は
+    else{
+        if(!$(".hide-btn").length){       //サイト表示時にDownMoveクラスを一瞬付与させないためのクラス付け。hide-btnがなければ下記の動作を行う
+    $("#page-top").addClass("DownMove");  //DownMoveをクラス追加
+    $("#page-top").removeClass("UpMove"); //UpMoveを削除 
+    }
+  }
+}
 
-// $(function () {
-//   $(window).on("scroll", function () {
-//     const sliderHeight = $(".js-change-2").height();
-//     if (sliderHeight - 30 < $(this).scrollTop()) {
-//       $(".js-gnav").addClass("is-black");
-//     } else {
-//       $(".js-gnav").removeClass("is-black");
-//     }
-//   });
-// });
+// 画面をスクロールをしたら動かしたい場合の記述
+$(window).scroll(function () {
+  setFadeElement();/* スクロールした際の動きの関数を呼ぶ*/
+});
 
-// function changeColor() {
-//   var timing = 40; //変化するタイミングを微調整する
+// ページが読み込まれたらすぐに動かしたい場合の記述
+$(window).on('load', function () {
+  setFadeElement();/* スクロールした際の動きの関数を呼ぶ*/
+});
 
-//   var scrollY = window.pageYOffset;
-//   var body = document.body;
 
-//   var trigger1 = document.getElementById('js-section-1');
-//   var trigger2 = document.getElementById('js-section-2');
-//   var trigger3 = document.getElementById('js-section-3');
-//   var trigger4 = document.getElementById('js-section-4');
 
-//   var trigger1Y = trigger1.getBoundingClientRect().top; // ウィンドウ上からの要素の位置
-//   var trigger2Y = trigger2.getBoundingClientRect().top;
-//   var trigger3Y = trigger3.getBoundingClientRect().top;
-//   var trigger4Y = trigger4.getBoundingClientRect().top;
-
-//   // 白背景の時はbodyの.bg-is-blackを削除
-//   // 黒背景の時はbodyに.bt-is-blackを付与
-//   if(trigger2Y - timing > 0 && 0 >= trigger1Y - timing) {
-//     body.classList.remove('is-white');
-//   } else if(trigger4Y - timing > 0 && 0 >= trigger3Y - timing) {
-//     body.classList.remove('is-white');
-//   } else {
-//     body.classList.add('is-white');
-//   }
-// }
-
-// window.addEventListener('scroll', changeColor);
-
-// $(window).scroll(function () {
-//   if($(window).scrollTop() > 20) {
-//     $('.js-gnav').addClass('is-white');
-//   } else {
-//     $('.js-gnav').removeClass('is-white');
-//   }
-// });
+// #page-topをクリックした際の設定
+$('#page-top').click(function () {
+    $('body,html').animate({
+        scrollTop: 0//ページトップまでスクロール
+    }, 500);//ページトップスクロールの速さ。数字が大きいほど遅くなる
+    return false;//リンク自体の無効化
+});
